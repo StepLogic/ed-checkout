890bced8446f179c4a66ff4033bb9863295df3f7
steplogic <kojogyaase@gmail..com>
2023-03-30 14:44:29 +0000

    fixes to all pages

--- src/assets/theme/theme.jsx
+++ src/assets/theme/theme.jsx
@@ -1,8 +1,5 @@
 import { createTheme } from "@mui/material/styles";
 
-import PoppinsMedium from "@font/PoppinsMedium.ttf";
-import PoppinsMediumItalic from "@font/Poppins-MediumItalic.ttf";
-
 export const theme = createTheme({
   typography: {
     fontFamily: "Poppins",
@@ -56,26 +53,7 @@ export const theme = createTheme({
     },
   },
   components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-          @font-face {
-            font-family: 'Poppins';
-            font-style: normal;
-            font-display: swap;
-            font-weight: medium;
-            src: local('Poppins'), url(${PoppinsMedium}) format('ttf');
-            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-          },
-            @font-face {
-            font-family: 'Poppins';
-            font-style: italic;
-            font-display: swap;
-            font-weight: medium;
-            src: local('Poppins'), url(${PoppinsMediumItalic}) format('ttf');
-            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-          }
-        `,
-    },
+    MuiCssBaseline: {},
     MuiDesktopDatePicker: {
       styleOverrides: {
         root: {
