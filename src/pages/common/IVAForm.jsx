4f0fe4656314fd39756d8fec9843d82ec8fb4b0a
steplogic <kojogyaase@gmail..com>
2023-04-15 15:46:08 +0000

    update to address field

--- src/pages/common/IVAForm.jsx
+++ src/pages/common/IVAForm.jsx
@@ -1,28 +1,12 @@
-import { Box, Button, Checkbox, Typography } from "@mui/material";
-import React from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import cn from "classnames";
 import { TextField } from "@components/textfield";
-import Autocomplete from "@mui/material/Autocomplete";
-import NearMeIcon from "@mui/icons-material/NearMe";
-import * as _ from "lodash";
-import axios from "axios";
-import InputAdornment from "@mui/material/InputAdornment";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
-import ClearIcon from "@mui/icons-material/Clear";
-import { LoadingButton } from "@mui/lab";
-const TOMTOM_KEY = import.meta.env.VITE_TOMTOM_API;
+import AddressField from "@components/AddressField";
+import { Box, Button } from "@mui/material";
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+import { useFormik } from "formik";
+import React from "react";
+import * as Yup from "yup";
 
 const IVAForm = (props) => {
-  const [openSnackbar, setOpenSnackbar] = React.useState(false);
-  const [addressLoading, setAddressLoading] = React.useState(false);
-  const [autoComplete, setAutoComplete] = React.useState({ status: false });
   const formik = useFormik({
     initialValues: {
       denominazioneERagioneSociale: "",
@@ -42,88 +26,13 @@ const IVAForm = (props) => {
       PECDesitinatatio: Yup.string().required("Campo richiesto"),
     }),
   });
-  console.log("user", formik.errors, formik.touched);
-
-  const handleAddressChange = (value) => {
-    if (value !== "") {
-      const u = () => autoCompleteAddressHandler(value);
-      _.delay(u, 1000);
-    }
-  };
-  const geoLocate = () => {
-    setAddressLoading(true);
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
-        const url = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${TOMTOM_KEY}&language=it-IT&typeahead=true&limit=10&type=Address&lat=${position.coords.latitude}&lon=${position.coords.longitude}&radius=1000`;
-        console.log("url", url);
-
-        axios
-          .get(url)
-          .then((response) => {
-            setAutoComplete({
-              status: true,
-              response: response.data.results,
-            });
-            const l = response?.data?.results?.map(
-              (r) =>
-                `${r?.address.freeformAddress.split(",")[0]}${
-                  r?.address?.municipality
-                    ? " , " + r?.address?.municipality
-                    : ""
-                }${
-                  r?.address?.postalCode ? " , " + r?.address?.postalCode : ""
-                }`
-            );
-
-            if (l.length > 0) {
-              formik.setFieldValue("indirizzo", l[0]);
-            } else {
-              setOpenSnackbar(true);
-            }
-          })
-          .catch((er) => setOpenSnackbar(true));
-      });
-    } else {
-      console.log("Not Available");
-    }
-    setAddressLoading(false);
-  };
-
-  const autoCompleteAddressHandler = (value) => {
-    axios
-      .get(
-        `https://api.tomtom.com/search/2/geocode/${value}.json?key=${TOMTOM_KEY}&language=it-IT&typeahead=true&limit=10&type=Address&lat=41.9102415&lon=12.395915&radius=100000000`
-      )
-      .then((response) => {
-        setAutoComplete({
-          status: true,
-          response: response.data.results,
-        });
-      })
-      .catch((er) => formik.setFieldValue("indirizzo", value));
-  };
 
   return (
     <>
       <h1 className=" font-semibold text-center lg:text-start  leading-none text-edu-900 text-[24px] lg:text-3xl 3xl:text-4xl max:text-6xl  h-fit  w-full">
         Fornisci le informazioni della Partita IVA
       </h1>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={2000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      >
-        <Alert
-          onClose={() => setOpenSnackbar(false)}
-          severity="primary"
-          sx={{ width: "100%" }}
-        >
-          No Address Found
-        </Alert>
-      </Snackbar>
+
       <Box
         sx={{
           display: ["grid"],
@@ -200,98 +109,12 @@ const IVAForm = (props) => {
             }
           />
 
-          <Autocomplete
-            freeSolo
+          <AddressField
+            handleChange={formik.setFieldValue}
+            name="indirizzo"
+            handleBlur={formik.handleBlur}
             value={formik.values.indirizzo}
-            onChange={(e, v) => {
-              formik.setFieldValue("indirizzo", v);
-            }}
-            onInputChange={(e) => {
-              if (e != null) {
-                formik.setFieldValue("indirizzo", e.target.value);
-                handleAddressChange(e.target?.value);
-              }
-            }}
-            classes={{ clearIndicator: "!text-[#8065C9] h" }}
-            options={
-              autoComplete.response?.map(
-                (r) =>
-                  `${r?.address.freeformAddress.split(",")[0]}${
-                    r?.address?.municipality
-                      ? " , " + r?.address?.municipality
-                      : ""
-                  }${
-                    r?.address?.postalCode ? " , " + r?.address?.postalCode : ""
-                  }`
-              ) ?? ["Rome"]
-            }
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                placeholder="Città, indirizzo e CAP"
-                name="indirizzo"
-                className="overflow-hidden"
-                sx={{
-                  ".MuiFormHelperText-root ": {
-                    minHeight: "23px!important",
-                  },
-                }}
-                onBlur={formik.handleBlur}
-                error={formik.errors.indirizzo && formik.touched.indirizzo}
-                helperText={formik.touched.indirizzo && formik.errors.indirizzo}
-                InputProps={{
-                  ...params.InputProps,
-
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <div className="flex flex-row items-center relative">
-                        <Box
-                          sx={{
-                            "&>div": {
-                              top: "0!important",
-                              width: "2rem",
-                              height: "2rem",
-                            },
-                            "&>div>button": {
-                              top: "0!important",
-                              width: "2rem",
-                              height: "2rem",
-                              fontSize: "2rem",
-                              padding: "0",
-                              marginBottom: "1rem",
-                            },
-                            "&>div>button>svg": {
-                              fontSize: "2rem",
-                            },
-                          }}
-                          className="my-auto left-[2rem] absolute h-[2rem] w-[2rem] top-0 bottom-0"
-                        >
-                          {params.InputProps.endAdornment}
-                        </Box>
-
-                        {addressLoading ? (
-                          <CircularProgress className="w-[1.5rem] h-[1.5rem]" />
-                        ) : (
-                          <button
-                            type="button"
-                            className="active:text-[#B4B4B4] active:border-[#B4B4B4]"
-                            onClick={() => geoLocate()}
-                          >
-                            <NearMeIcon
-                              sx={{
-                                color: "#886FCC",
-                                fontSize: "2rem",
-                                marginRight: "1rem",
-                              }}
-                            />
-                          </button>
-                        )}
-                      </div>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            )}
+            error={formik.touched.indirizzo && formik.errors.indirizzo}
           />
           <TextField
             placeholder="PEC destinatario"
--- src/pages/common/UserInfo.jsx
+++ src/pages/common/UserInfo.jsx
@@ -1,26 +1,15 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  FormControlLabel,
-  Link,
-  Typography,
-} from "@mui/material";
-import Autocomplete from "@mui/material/Autocomplete";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import cn from "classnames";
+import AddressField from "@components/AddressField";
 import { TextField } from "@components/textfield";
-import InputAdornment from "@mui/material/InputAdornment";
-import NearMeIcon from "@mui/icons-material/NearMe";
-import Snackbar from "@mui/material/Snackbar";
-import ClearIcon from "@mui/icons-material/Clear";
+import { Box, Button, Checkbox, FormControlLabel, Link } from "@mui/material";
 import MuiAlert from "@mui/material/Alert";
-import CircularProgress from "@mui/material/CircularProgress";
+import Snackbar from "@mui/material/Snackbar";
+import axios from "axios";
+import cn from "classnames";
+import { useFormik } from "formik";
 import * as _ from "lodash";
-import { LoadingButton } from "@mui/lab";
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
+
 const TOMTOM_KEY = import.meta.env.VITE_TOMTOM_API;
 
 const Alert = React.forwardRef(function Alert(props, ref) {
@@ -29,9 +18,6 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 
 const UserInfo = ({ product, next }) => {
   const [checked, setChecked] = useState(false);
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [addressLoading, setAddressLoading] = useState(false);
-  const [autoComplete, setAutoComplete] = useState({ status: false });
 
   const formik = useFormik({
     initialValues: {
@@ -56,86 +42,11 @@ const UserInfo = ({ product, next }) => {
     }),
   });
 
-  const handleAddressChange = (value) => {
-    if (value !== "") {
-      const u = () => autoCompleteAddressHandler(value);
-      _.delay(u, 1000);
-    }
-  };
-
-  const geoLocate = () => {
-    setAddressLoading(true);
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
-        const url = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${TOMTOM_KEY}&language=it-IT&typeahead=true&limit=10&type=Address&lat=${position.coords.latitude}&lon=${position.coords.longitude}&radius=1000`;
-        console.log("url", url);
-
-        axios
-          .get(url)
-          .then((response) => {
-            setAutoComplete({
-              status: true,
-              response: response.data.results,
-            });
-            const l = response?.data?.results?.map(
-              (r) =>
-                `${r?.address.freeformAddress.split(",")[0]}${
-                  r?.address?.municipality
-                    ? " , " + r?.address?.municipality
-                    : ""
-                }${
-                  r?.address?.postalCode ? " , " + r?.address?.postalCode : ""
-                }`
-            );
-
-            if (l.length > 0) {
-              formik.setFieldValue("indirizzo", l[0]);
-            } else {
-              setOpenSnackbar(true);
-            }
-          })
-          .catch((er) => setOpenSnackbar(true));
-      });
-    } else {
-      console.log("Not Available");
-    }
-    setAddressLoading(false);
-  };
-  const autoCompleteAddressHandler = (value) => {
-    axios
-      .get(
-        `https://api.tomtom.com/search/2/geocode/${value}.json?key=${TOMTOM_KEY}&language=it-IT&typeahead=true&limit=10&type=Address&lat=41.9102415&lon=12.395915&radius=100000000`
-      )
-      .then((response) => {
-        setAutoComplete({
-          status: true,
-          response: response.data.results,
-        });
-      })
-      .catch((er) => formik.setFieldValue("indirizzo", value));
-  };
-
   return (
     <>
       <h1 className=" font-semibold text-center lg:text-start  leading-none text-edu-900 text-[24px] lg:text-3xl 3xl:text-4xl max:text-6xl w-full">
         Finalizza la tua iscrizione
       </h1>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={2000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      >
-        <Alert
-          onClose={() => setOpenSnackbar(false)}
-          severity="primary"
-          sx={{ width: "100%" }}
-        >
-          No Address Found
-        </Alert>
-      </Snackbar>
 
       <Box
         component="form"
@@ -197,98 +108,13 @@ const UserInfo = ({ product, next }) => {
           helperText={formik.touched.email && formik.errors.email}
           error={formik.touched.email && formik.errors.email}
         />
-        <Autocomplete
-          freeSolo
-          onChange={(e, v) => {
-            formik.setFieldValue("indirizzo", v);
-          }}
-          onInputChange={(e) => {
-            if (e != null) {
-              formik.setFieldValue("indirizzo", e.target.value);
-              handleAddressChange(e.target?.value);
-            }
-          }}
+
+        <AddressField
+          handleChange={formik.setFieldValue}
+          name="indirizzo"
+          handleBlur={formik.handleBlur}
           value={formik.values.indirizzo}
-          clearIcon={<ClearIcon color="primary" />}
-          options={
-            autoComplete.response?.map(
-              (r) =>
-                `${r?.address.freeformAddress.split(",")[0]}${
-                  r?.address?.municipality
-                    ? " , " + r?.address?.municipality
-                    : ""
-                }${
-                  r?.address?.postalCode ? " , " + r?.address?.postalCode : ""
-                }`
-            ) ?? []
-          }
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              placeholder="Inserisci indirizzo"
-              name="indirizzo"
-              sx={{
-                ".MuiFormHelperText-root ": {
-                  minHeight: "23px!important",
-                },
-              }}
-              onBlur={formik.handleBlur}
-              error={formik.errors.indirizzo && formik.touched.indirizzo}
-              helperText={formik.touched.indirizzo && formik.errors.indirizzo}
-              className="overflow-hidden"
-              InputProps={{
-                ...params.InputProps,
-
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <div className="flex flex-row items-center relative">
-                      <Box
-                        sx={{
-                          "&>div": {
-                            top: "0!important",
-                            width: "2rem",
-                            height: "2rem",
-                          },
-                          "&>div>button": {
-                            top: "0!important",
-                            width: "2rem",
-                            height: "2rem",
-                            fontSize: "2rem",
-                            padding: "0",
-                            marginBottom: "1rem",
-                          },
-                          "&>div>button>svg": {
-                            fontSize: "2rem",
-                          },
-                        }}
-                        className="my-auto left-[2rem] absolute h-[2rem] w-[2rem] top-0 bottom-0"
-                      >
-                        {params.InputProps.endAdornment}
-                      </Box>
-
-                      {addressLoading ? (
-                        <CircularProgress className="w-[1.5rem] h-[1.5rem]" />
-                      ) : (
-                        <button
-                          type="button"
-                          className="active:text-[#B4B4B4] active:border-[#B4B4B4]"
-                          onClick={() => geoLocate()}
-                        >
-                          <NearMeIcon
-                            sx={{
-                              color: "#886FCC",
-                              fontSize: "2rem",
-                              marginRight: "1rem",
-                            }}
-                          />
-                        </button>
-                      )}
-                    </div>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          )}
+          error={formik.touched.indirizzo && formik.errors.indirizzo}
         />
       </Box>
 
